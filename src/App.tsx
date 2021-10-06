102474c93c4398848e3202c308bb08bb35c5cfe1
Denis Coelho <deniscoelho.dev@gmail.com>
2021-10-06 15:20:58 -0300

    mod 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 import * as C from './App.styles'
 import './App.css';
+import {Item} from './types/'
 
 function App() {
+
+  const [list, setList] = useState([]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>
+          Lista de tarefas
+        </C.Header>
+      </C.Area>
+    </C.Container>
   );
 }
 
